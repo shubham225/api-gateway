15879f742a9d478873254631032acc41df318c65
Shubham Shinde <shubhamshinde225@gmail.com>
2024-03-02 22:15:12 +0530

    minor changes done

--- src/main/java/com/services/gateway/security/SecurityConfig.java
+++ src/main/java/com/services/gateway/security/SecurityConfig.java
@@ -1,24 +1,10 @@
 package com.services.gateway.security;
 
-import org.springframework.boot.actuate.autoconfigure.endpoint.web.WebEndpointProperties;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.http.MediaType;
 import org.springframework.security.config.Customizer;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
-import org.springframework.security.core.userdetails.MapReactiveUserDetailsService;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.LoginUrlAuthenticationEntryPoint;
 import org.springframework.security.web.server.SecurityWebFilterChain;
-import org.springframework.security.web.util.matcher.MediaTypeRequestMatcher;
-
-import static org.springframework.security.config.Customizer.withDefaults;
 
 @EnableWebFluxSecurity
 public class SecurityConfig {
